41957211d8dd040751a0190ce74f9f10a5d0b922
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2021-09-02 18:50:54 -0600

    Update Semana 2_Estructuras.R

---
+++ Semana 2_Estructuras.R
@@ -0,0 +1,17 @@
+#Se puede aplicar funciones aritmeticas 
+x=1:10
+sqrt(x)
+
+#Alterarlo por medio de procedimientos 
+b= (10+2)*(1:10)
+b
+
+#Funciones de los vectores
+c= 1:30
+length(c)
+max(c)
+min(c)
+sum(c)
+mean(c)
+sort(c, dec=TRUE)
+rev(c)
