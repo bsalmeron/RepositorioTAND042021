f26452c95ac1d7a194e80dfde7e61ebd83868c45
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2021-09-07 20:41:42 -0600

    Update Practica 2.R

--- Practica 2.R
+++ Practica 2.R
@@ -1,5 +1,5 @@
 
 #Parte 2 caso estadistico
 genero = c("F","M","F","F","M","F","M","F","F","M")
-Edad = sample(1:30,10)#aleatorios 
-
+Edad = sample(1:30,10) #aleatorios 
+Glucemia_basal = c(123,230,180,164,210,220,193,202,152,185) 
