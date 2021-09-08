0cca509a449ce4eb9cae66f990965ab224887101
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2021-09-07 20:45:28 -0600

    Update Practica 2.R

--- Practica 2.R
+++ Practica 2.R
@@ -4,3 +4,6 @@ genero = c("F","M","F","F","M","F","M","F","F","M")
 Edad = sample(1:30,10) #aleatorios 
 Glucemia_basal = c(123,230,180,164,210,220,193,202,152,185) 
 Tratamiento=c(TRUE,FALSE,TRUE,TRUE,FALSE,FALSE,TRUE,TRUE,FALSE,TRUE)
+
+#Usar la funcion data.frame para crear el dataframe
+mispacientes=data.frame(genero,Edad,Glucemia_basal,Tratamiento)
