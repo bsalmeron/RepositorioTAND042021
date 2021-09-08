8fe4add796ea3170fc6a6354c4020a769c411538
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2021-09-07 20:43:41 -0600

    Update Practica 2.R

--- Practica 2.R
+++ Practica 2.R
@@ -3,3 +3,4 @@
 genero = c("F","M","F","F","M","F","M","F","F","M")
 Edad = sample(1:30,10) #aleatorios 
 Glucemia_basal = c(123,230,180,164,210,220,193,202,152,185) 
+Tratamiento=c(TRUE,FALSE,TRUE,TRUE,FALSE,FALSE,TRUE,TRUE,FALSE,TRUE)
